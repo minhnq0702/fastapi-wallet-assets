9a9c30213d2d0594e3968b68798305cd4d1c7b7b
MingNe <minhnq.0702@gmail.com>
2024-08-12 14:23:24 +0700

    perf: decorator inject db session

--- app/repo/database.py
+++ app/repo/database.py
@@ -1,15 +1,17 @@
 """Init database connection"""
 # -*- coding: utf-8 -*-
+import typing
+from functools import wraps
 from typing import Any, AsyncGenerator
 
-from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlmodel import MetaData, SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 # TODO: load config from .env
-DATABASE_URL = "postgresql+asyncpg://minhnguyen:@localhost/wallet-assets"
+DATABASE_URI = "postgresql+asyncpg://minhnguyen:@localhost/wallet-assets"
 # db = Database(DATABASE_URL)
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URI, echo=True)
 
 DBMetadata = MetaData()
 DBSession = async_sessionmaker(
@@ -18,6 +20,16 @@ DBSession = async_sessionmaker(
     bind=engine,
     class_=AsyncSession
 )
+
+
+async def init_models():
+    """
+    Init Database models meta
+    """
+    async with engine.begin() as conn:
+        # await conn.run_sync(SQLModel.metadata.drop_all)
+        # * Run create all SQLModel into database
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 
 async def get_tx() -> AsyncGenerator[AsyncSession, Any]:
@@ -29,3 +41,28 @@ async def get_tx() -> AsyncGenerator[AsyncSession, Any]:
     async with DBSession() as _session:
         # async with _session.begin():
         yield _session
+
+R = typing.TypeVar('R')
+P = typing.ParamSpec('P')
+
+
+def with_session(
+        func: typing.Callable[..., typing.Coroutine[Any, Any, R]]
+    ) -> typing.Callable[..., typing.Coroutine[Any, Any, R]]:
+    """Wrapper get db session into kwargs
+
+    Args:
+        func (typing.Callable[..., typing.Coroutine[Any, Any, Any]]): _description_
+
+    Returns:
+        typing.Callable[..., typing.Coroutine[Any, Any, Any]]: _description_
+    """
+    @wraps(func)
+    async def func_wrap(*args: P.args, **kwargs: P.kwargs):
+        """
+        Wrap function with db session
+        """
+        async with DBSession() as _session:
+            res = await func(*args, session=_session, **kwargs)
+            return res
+    return func_wrap
